0264ffe3e84df7c3265f7f50ce8afdd648ba65c3
kfiddle <kenjfiddle@gmail.com>
2021-05-06 19:34:10 -0400

    contact info box

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -5,26 +5,44 @@ export const ContactBox = {
   contactBox: BetterElement("div", "contactBox"),
   isOpen: false,
 
+  submitContactInfo: function() {
+    let contactInfo = {
+      fullName: document.querySelector('.nameInput').value,
+      email: document.querySelector('.emailInput').value,
+      phoneNumber: document.querySelector('.phoneInput').value,
+      message: document.querySelector('.messageInput').value,
+    }
+
+
+    console.log(contactInfo);
+
+  },
+
   open: function () {
     const inputsBox = BetterElement("div");
     const contactBoxHeader = BetterElement("div");
     const header = BetterElement("h2");
-    header.innerHTML =
+    header.innerText =
       "Feel free to share as much information as you'd like here";
     const myContactInfoBox = BetterElement("div", "myContactInfo");
     const myContactInfoSentence = BetterElement("h2");
-    myContactInfoSentence.innerHTML = "Or simply write me at...";
+    myContactInfoSentence.innerText = "Or simply write me at...";
     const myEmail = BetterElement("h1", "myEmail");
-    myEmail.innerHTML = "@SuperDuperEmail";
+    myEmail.innerText = "kenjsoftware@gmail.com";
+    const messageInput = BetterElement('textArea', 'messageInput');
+    messageInput.setAttribute('cols', '60');
+    messageInput.setAttribute('rows', '10')
+    
 
     const inputs = {
-      nameInput: BetterElement("input"),
-      emailInput: BetterElement("input"),
-      phoneInput: BetterElement("input"),
+      nameInput: BetterElement("input", 'nameInput'),
+      emailInput: BetterElement("input", 'emailInput'),
+      phoneInput: BetterElement("input", 'phoneInput'),
     };
 
     const submitButton = BetterElement("button", "submitButton");
-    submitButton.innerText = "Submit";
+    submitButton.innerHTML = `<h2>Submit</h2>`;
+    submitButton.addEventListener('click', this.submitContactInfo);
 
     contactBoxHeader.id = "contactBoxHeader";
     inputs.nameInput.id = "nameInput";
@@ -40,13 +58,15 @@ export const ContactBox = {
     }
 
     myContactInfoBox.appendChild(myContactInfoSentence);
-    this.contactBox.appendChild(submitButton);
-
+    
     this.contactBox.appendChild(myContactInfoBox).appendChild(myEmail);
     this.contactBox.appendChild(contactBoxHeader).appendChild(header);
     app
-      .appendChild(this.contactBox)
-      .appendChild(inputsBox);
+    .appendChild(this.contactBox)
+    .appendChild(inputsBox);
+    this.contactBox.appendChild(messageInput);
+    this.contactBox.appendChild(submitButton);
+
 
     waitAndThen(() => {
       this.contactBox.style.transform = "translateY(-110vh)";
@@ -59,7 +79,7 @@ export const ContactBox = {
     }
 
     waitAndThen(() => {
-      myContactInfoBox.style.transform = "translateY(45vh)";
+      myContactInfoBox.style.transform = "translateY(60vh)";
     }, 1000);
 
     this.isOpen = true;
