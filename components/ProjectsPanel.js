0ceff18c93714aab40d4350a15647236bd4310f0
kfiddle <kenjfiddle@gmail.com>
2021-05-08 20:39:30 -0400

    start of project modal

--- components/ProjectsPanel.js
+++ components/ProjectsPanel.js
@@ -2,22 +2,94 @@ import { BetterElement, waitAndThen } from "./betterElement.js";
 
 let app = document.getElementById("app");
 
+class ProjectBox {
+  constructor(tag, ...classList) {
+    this.box = BetterElement(tag, ...classList);
+
+    this.box.addEventListener("mouseover", () => {
+      this.highlightProject("hover");
+    });
+    this.box.addEventListener("mouseout", () => {
+      this.highlightProject("hoverOut");
+    });
+    this.box.addEventListener("click", () => {
+      this.openProjectModal();
+    });
+  }
+
+  highlightProject(mouseAction) {
+    if (mouseAction === "hover") {
+      this.box.style.filter =
+        "brightness(80%) hue-rotate(200deg) saturate(60%) invert(20%)";
+      let innerProjectHeader = BetterElement("h2", "innerProjectHeader");
+      let clickForDeets = "click for details";
+      let lettersIndex = 0;
+
+      function oneLetter() {
+        function changeALetter(timestamp) {
+          if (lettersIndex < clickForDeets.length + 1) {
+            innerProjectHeader.innerText = clickForDeets.substr(
+              0,
+              lettersIndex
+            );
+            lettersIndex++;
+          }
+          requestAnimationFrame(oneLetter);
+        }
+        requestAnimationFrame(changeALetter);
+      }
+
+      oneLetter();
+
+      this.box.appendChild(innerProjectHeader);
+    } else {
+      this.box.removeChild(this.box.lastChild);
+      this.box.style.filter = "";
+    }
+  }
+
+  openProjectModal() {
+    // console.log(this.box.classList);
+
+    let modalBox = BetterElement("div", "projectModal");
+    let projectsDiv = document.querySelector(".projectsPanel");
+    
+    let xOut = BetterElement('h1', 'xOut');
+    xOut.innerText = 'X';
+    modalBox.appendChild(xOut);
+    
+    
+    projectsDiv.appendChild(modalBox);
+    waitAndThen(() => {
+      modalBox.rollout("translateY(-100vh)");
+    }, 200);
+  }
+}
+
 export const ProjectComponents = {
   projectsContainer: BetterElement("div", "projectsPanel"),
   projectBoxes: {
-    1: BetterElement("div", "projectBox", "first"),
-    2: BetterElement("div", "projectBox", "second"),
-    3: BetterElement("div", "projectBox", "third"),
-    4: BetterElement("div", "projectBox", "fourth"),
+    1: new ProjectBox("div", "projectBox", "first"),
+    2: new ProjectBox("div", "projectBox", "second"),
+    3: new ProjectBox("div", "projectBox", "third"),
+    4: new ProjectBox("div", "projectBox", "fourth"),
+  },
+
+  rollTheBoxes: function (direction) {
+    for (let project in this.projectBoxes) {
+      direction === "in"
+        ? this.projectBoxes[project].box.rollout("translateY(-100vh)")
+        : this.projectBoxes[project].box.rollout("translateY(100vh)");
+    }
   },
 
   isOpen: false,
 
   open: function () {
-    for (let box in this.projectBoxes) {
-      this.projectsContainer.appendChild(this.projectBoxes[box]);
+    for (let project in this.projectBoxes) {
+      this.projectsContainer.appendChild(this.projectBoxes[project].box);
       waitAndThen(() => {
-        this.projectBoxes[box].rollout("translateY(-100vh)");
+        this.rollTheBoxes("in");
       }, 200);
     }
 
@@ -27,14 +99,24 @@ export const ProjectComponents = {
   },
 
   close: function () {
-    for (let box in this.projectBoxes) {
-      this.projectBoxes[box].rollout("translateY(100vh)");
+    this.rollTheBoxes("out");
+
+    if (document.querySelector(".projectModal")) {
+      this.closeModal();
     }
 
     waitAndThen(() => {
       app.removeChild(this.projectsContainer);
-    }, 200);
+    }, 400);
 
     this.isOpen = false;
+  },
+
+  closeModal: function () {
+    let modal = this.projectsContainer.querySelector(".projectModal");
+    modal.rollout("translateY(0vh)");
+    waitAndThen(() => {
+      this.projectsContainer.removeChild(modal);
+    }, 200);
   },
 };
