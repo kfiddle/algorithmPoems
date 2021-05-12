4afac8e3b5ed751832912d4621790153e0b7f7b1
kfiddle <kenjfiddle@gmail.com>
2021-05-12 17:34:13 -0400

    almost

--- components/ProjectModal.js
+++ components/ProjectModal.js
@@ -1,43 +1,54 @@
 import { BetterElement, waitAndThen } from "./betterElement.js";
 
 let app = document.getElementById("app");
-
+let carouselIndex = 0;
 
 let projectInfo = [
-  { description: 'A collaboration with my friend Karin Samoviski. All animations and components are vanilla Javascript, and the back-end modeling and database are built in Java and Spring Boot. ',
-    carousel:['/assets/slides/skyPondSlide1.jpg', '/assets/slides/skyPondSlide2.jpg', '/assets/slides/skyPondPhoneFormatter.jpg' ]
+  {
+    description:
+      "A collaboration with my friend Karin Samoviski. All animations and components are vanilla Javascript, and the back-end modeling and database are built in Java and Spring Boot. ",
+    carousel: [
+      "/assets/slides/skyPondSlide1.jpg",
+      "/assets/slides/skyPondSlide2.jpg",
+      "/assets/slides/skyPondPhoneFormatter.jpg",
+    ],
   },
   {
-    description:'An app designed for genealogists. Perhaps your ancestor inherited land in 1750? Or was paid by the colonial army per mile of marching? This app will help you do a few necessary calculations with an unfamiliar currency.',
-    carousel:['/assets/slides/colonialCurrencySlide1.jpg']
+    description:
+      "An app designed for genealogists. Perhaps your ancestor inherited land in 1750? Or was paid by the colonial army per mile of marching? This app will help you do a few necessary calculations with an unfamiliar currency.",
+    carousel: [
+      "/assets/slides/colonialCurrencySlide1.jpg",
+      "/assets/slides/colonialSlide2.jpg",
+    ],
   },
   {
-    description:'A business simulation. In this case, a generic employee survey form populates an independent spreadsheet, and a relational database of companies and their employees is managed through the administrative pages. Java Spring Boot and Javascript.',
-    carousel:['/assets/slides/apexSlide1.jpg']
-
+    description:
+      "A business simulation. In this case, a generic employee survey form populates an independent spreadsheet, and a relational database of companies and their employees is managed through the administrative pages. Java Spring Boot and Javascript.",
+    carousel: ["/assets/slides/apexSlide1.jpg"],
   },
   {
-    description:'a super DUPER cool thing',
-    carousel:['/assets/slides/apexSlide1.jpg']
-  }
-]
-            
+    description: "a super DUPER cool thing",
+    carousel: ["/assets/slides/apexSlide1.jpg"],
+  },
+];
 
 export const ProjectModal = {
-
-  isOpen: false,
-  modalBox: BetterElement("div", "projectModal"),
-  innerModalDiv: BetterElement("div", "innerModalDiv"),
-
-  descriptionDiv: BetterElement("div", "descriptionDiv"),
-  descriptionText: BetterElement("h3", "descriptionText"),
-
-  slideDiv: BetterElement("div", "slideDiv"),
-  slideImageDiv: BetterElement('div', 'slideImageDiv'),
-
-  slideButtons: [BetterElement("button", "carouselButtons", "arrow-left"), BetterElement("button", "carouselButtons", "arrow-right")],
-
   open: function (projectIndex) {
+    this.isOpen = true;
+    this.modalBox = BetterElement("div", "projectModal");
+    this.innerModalDiv = BetterElement("div", "innerModalDiv");
+
+    this.descriptionDiv = BetterElement("div", "descriptionDiv");
+    this.descriptionText = BetterElement("h3", "descriptionText");
+
+    this.slideDiv = BetterElement("div", "slideDiv");
+    this.slideImageDiv = BetterElement("div", "slideImageDiv");
+
+    this.slideButtons = [
+      BetterElement("button", "carouselButtons", "arrow-left"),
+      BetterElement("button", "carouselButtons", "arrow-right"),
+    ];
+
     let xOut = BetterElement("h1", "xOut");
     xOut.innerText = "X";
     xOut.addEventListener("click", () => {
@@ -50,14 +61,17 @@ export const ProjectModal = {
     this.descriptionDiv.appendChild(this.descriptionText);
     this.innerModalDiv.appendChild(this.descriptionDiv);
 
-    this.slideImageDiv.style.backgroundImage = `url(${projectInfo[projectIndex].carousel[0]})`;
+    this.slideImageDiv.id = "slideImageDiv";
+    this.slideImageDiv.style.backgroundImage = `url(${projectInfo[projectIndex].carousel[carouselIndex]})`;
     this.slideDiv.appendChild(this.slideImageDiv);
-    
+
     this.slideButtons.forEach((button, index) => {
-      button.addEventListener('click', ()=> { moveSlides(index) })
+      button.addEventListener("click", () => {
+        moveSlides(index, projectIndex);
+      });
       this.slideDiv.appendChild(button);
     });
-  
+
     this.innerModalDiv.appendChild(this.slideDiv);
     this.modalBox.appendChild(this.innerModalDiv);
 
@@ -71,27 +85,45 @@ export const ProjectModal = {
     }, 800);
 
     this.isOpen = true;
+    carouselIndex = 0;
   },
 
   close: function () {
     if (this.isOpen) {
       this.modalBox.rollout("translateX(100vw)");
       this.modalBox.classList.remove("opened");
+      this.slideButtons.forEach((button) =>
+        button.removeEventListener("click", () => {
+          moveSlides(index, projectIndex);
+        })
+      );
       waitAndThen(() => {
         this.modalBox.eraseAllKids();
         this.modalBox.parentElement.removeChild(this.modalBox);
+        console.log(document.getElementById("app"));
       }, 500);
       this.isOpen = false;
+
+      carouselIndex = 0;
     }
   },
 };
 
+function moveSlides(leftOrRightButton, projectIndex) {
+  let slideImageDiv = document.getElementById("slideImageDiv");
+  let listLength = projectInfo[projectIndex].carousel.length;
 
-function moveSlides(leftOrRightButton) {
-  let leftButton = document.getElementsByClassName('arrow-left');
-  let rightButton = document.getElementsByClassName('arrow-right');
-
-  console.log(leftOrRightButton);
-
-
+  if (leftOrRightButton === 1) {
+    if (carouselIndex === listLength - 1) {
+      carouselIndex = -1;
+    }
+    carouselIndex++;
+  } else {
+    if (carouselIndex === 0) {
+      carouselIndex = listLength;
+    }
+    carouselIndex--;
+  }
+  slideImageDiv.style.backgroundImage = `url(${projectInfo[projectIndex].carousel[carouselIndex]})`;
+  console.log(carouselIndex);
 }
